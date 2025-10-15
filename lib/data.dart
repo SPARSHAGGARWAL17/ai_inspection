67c622b9a7c2bf39b646887955807105b94d7e59
SPARSHAGGARWAL17 <aggarwalsparsh175@gmail.com>
2025-10-15 17:53:36 +0530

    Feat: change upload section name and limitations

--- lib/data.dart
+++ lib/data.dart
@@ -14,17 +14,12 @@ class AppStaticData {
     // •⁠  ⁠Backyard (5-10 photos)
     // •⁠  ⁠Garage (5-10 photos)
     // •⁠  ⁠Pool (4-7 photos)
-    FileUploadSection(sectionId: 'fronthouse', sectionName: 'Front with house number', maxPhotoAllowed: 2),
-    FileUploadSection(sectionId: 'backside', sectionName: 'Backside Photo', maxPhotoAllowed: 4),
-    FileUploadSection(sectionId: 'roof', sectionName: 'Roof', maxPhotoAllowed: 20),
-    FileUploadSection(sectionId: 'chimney', sectionName: 'Chimney', maxPhotoAllowed: 10),
-    FileUploadSection(sectionId: 'gutter', sectionName: 'Gutter', maxPhotoAllowed: 10),
-    FileUploadSection(sectionId: 'windows', sectionName: 'Windows', maxPhotoAllowed: 15),
-    FileUploadSection(sectionId: 'doors', sectionName: 'Doors', maxPhotoAllowed: 15),
-    FileUploadSection(sectionId: 'siding', sectionName: 'Siding/Wall', maxPhotoAllowed: 15),
-    FileUploadSection(sectionId: 'fence', sectionName: 'Fence', maxPhotoAllowed: 10),
-    FileUploadSection(sectionId: 'backyard', sectionName: 'Backyard', maxPhotoAllowed: 10),
-    FileUploadSection(sectionId: 'garage', sectionName: 'Garage', maxPhotoAllowed: 10),
-    FileUploadSection(sectionId: 'pool', sectionName: 'Pool', maxPhotoAllowed: 7),
+    FileUploadSection(sectionId: 'roof', sectionName: 'Roof', maxPhotoAllowed: 8),
+    FileUploadSection(sectionId: 'fronthouse', sectionName: 'Front with house number', maxPhotoAllowed: 8),
+    FileUploadSection(sectionId: 'right', sectionName: 'Right Side', maxPhotoAllowed: 8),
+    FileUploadSection(sectionId: 'left', sectionName: 'Left Side', maxPhotoAllowed: 8),
+    FileUploadSection(sectionId: 'rear', sectionName: 'Rear Side', maxPhotoAllowed: 8),
+    FileUploadSection(sectionId: 'fence', sectionName: 'Fence', maxPhotoAllowed: 8),
+    FileUploadSection(sectionId: 'extra', sectionName: 'Extras', maxPhotoAllowed: 10),
   ];
 }
